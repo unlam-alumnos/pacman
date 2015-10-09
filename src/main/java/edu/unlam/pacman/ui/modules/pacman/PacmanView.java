982d45490c280a011e376427864f78700d286513
gonzalojmartin <gonzalojmartin@hotmail.com>
2015-10-09 20:08:55 -0300

    Keylistener implementado

--- src/main/java/edu/unlam/pacman/ui/modules/pacman/PacmanView.java
+++ src/main/java/edu/unlam/pacman/ui/modules/pacman/PacmanView.java
@@ -1,16 +1,14 @@
 package edu.unlam.pacman.ui.modules.pacman;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.KeyEventDispatcher;
-import java.awt.KeyboardFocusManager;
-import java.awt.event.KeyEvent;
-
 import edu.unlam.pacman.common.Direction;
 import edu.unlam.pacman.ui.mvp.UiHandler;
 import edu.unlam.pacman.ui.mvp.View;
 
-public class PacmanView extends View<PacmanView.MyView> {
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PacmanView extends View<PacmanView.MyView> implements KeyListener {
     interface MyView extends UiHandler {
         void move(Direction direction);
         void paintPacman();
@@ -20,7 +18,7 @@ public class PacmanView extends View<PacmanView.MyView> {
     protected void onBind() {
         setOpaque(false);
         setFocusable(true);
-        bindKeys();
+        addKeyListener(this);
     }
 
     @Override
@@ -41,21 +39,26 @@ public class PacmanView extends View<PacmanView.MyView> {
         graphics().fillArc(x, y, width, height, angle, 300);
     }
 
-    private void bindKeys() {
-        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
-            @Override
-            public boolean dispatchKeyEvent(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_UP) {
-                    uiHandler().move(Direction.UP);
-                } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    uiHandler().move(Direction.RIGHT);
-                } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    uiHandler().move(Direction.LEFT);
-                } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    uiHandler().move(Direction.DOWN);
-                }
-                return false;
-            }
-        });
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            uiHandler().move(Direction.UP);
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            uiHandler().move(Direction.RIGHT);
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            uiHandler().move(Direction.LEFT);
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            uiHandler().move(Direction.DOWN);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
