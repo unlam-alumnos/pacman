b374f6636c5df336617aa43ea3ee11007ed8d993
gonzalojmartin <gonzalojmartin@hotmail.com>
2015-10-23 08:42:39 -0300

    HunterEvent

---
+++ src/main/java/edu/unlam/pacman/shared/comunication/bus/events/HunterEvent.java
@@ -0,0 +1,11 @@
+package edu.unlam.pacman.shared.comunication.bus.events;
+
+/**
+ * Created by gmartin on 22/10/2015.
+ */
+public class HunterEvent extends GameEvent {
+
+    public HunterEvent(String subject) {
+        super(subject);
+    }
+}
---
+++ src/main/java/edu/unlam/pacman/shared/model/Status.java
@@ -0,0 +1,9 @@
+package edu.unlam.pacman.shared.model;
+
+/**
+ * @author Cristian Miranda
+ * @since 10/7/15 - 20:57
+ */
+public enum Status {
+    NORMAL, HUNTER, VICTIM
+}
