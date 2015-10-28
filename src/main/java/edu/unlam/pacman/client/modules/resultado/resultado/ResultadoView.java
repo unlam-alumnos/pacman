140b70d412add72bea07bdbd0b001ddb8ce3e345
Gonzalo Martin <gonzalojmartin@hotmail.com>
2015-10-28 17:35:32 -0300

    Pantalla resultado utilizar sprites

--- src/main/java/edu/unlam/pacman/client/modules/resultado/resultado/ResultadoView.java
+++ src/main/java/edu/unlam/pacman/client/modules/resultado/resultado/ResultadoView.java
@@ -4,9 +4,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 
-import javax.swing.JLabel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import edu.unlam.pacman.client.modules.juego.JuegoConstants;
 import edu.unlam.pacman.client.modules.resultado.ResultadoConstants;
 import edu.unlam.pacman.client.mvp.UiHandler;
 import edu.unlam.pacman.client.mvp.View;
@@ -26,6 +27,7 @@ public class ResultadoView extends View<ResultadoView.MyView> {
         setBorder(new EmptyBorder(5, 5, 5, 5));
         setLayout(null);
 
+        /*
         JLabel lbGanador = new JLabel("GANADOR:");
         lbGanador.setForeground(Color.WHITE);
         lbGanador.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -49,11 +51,47 @@ public class ResultadoView extends View<ResultadoView.MyView> {
         lbTiempoValor.setFont(new Font("Tahoma", Font.PLAIN, 20));
         lbTiempoValor.setBounds(218, 147, 150, 25);
         add(lbTiempoValor);
+        */
+
     }
 
     @Override
     public void paintComponent(Graphics2D g2) {
 
+        // TODO: esto se ejecuta constantemente, podemos adaptarlo para que se imprima cuando se crea unicamente ¿?
+
+        ImageIcon sprite = new ImageIcon(JuegoConstants.SPRITES_PATH + "/letras.png");
+        String texto = "ganador usuario";
+        int sizeLetra = 25;
+        int y = 140;
+        int dirImgY = 0;
+        int tamImgY = 8;
+        int espacio = 0;
+
+        for (int i = 0; i < texto.length(); i++){
+            int letra = (int)texto.charAt(i);
+            int x = 10 + espacio + i*sizeLetra;
+            if (letra == 32){
+                espacio = 12;
+            }else{
+                graphics().drawImage(sprite.getImage(), x, y, x+sizeLetra, y+25, 8*(letra-97), dirImgY, 8*(letra-96), tamImgY , this);
+            }
+        }
+
+        texto = "tiempo xx segundos";
+        y = 180;
+        espacio = 0;
+        for (int i = 0; i < texto.length(); i++){
+            int letra = (int)texto.charAt(i);
+            int x = 10 + espacio + i*sizeLetra;
+            if (letra == 32){
+                espacio = 12;
+            }else{
+                graphics().drawImage(sprite.getImage(), x, y, x+sizeLetra, y+25, 8*(letra-97), dirImgY, 8*(letra-96), tamImgY , this);
+            }
+        }
+        // alt 97 --> a
+        // alt 122 --> z
     }
 
     @Override
--- src/test/java/edu/unlam/pacman/server/service/JugadorServiceTest.java
+++ src/test/java/edu/unlam/pacman/server/service/JugadorServiceTest.java
@@ -13,7 +13,6 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 /**
- *
  * @author Cristian Miranda
  * @since 10/11/15 - 18:26
  */
